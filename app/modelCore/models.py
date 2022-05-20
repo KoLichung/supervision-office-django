a8720485ac3d1245e2de1f13e8e0d5f59fd3bdad
JasonKo <jason@kosbrother.com>
2022-05-20 15:00:25 +0800

    alter model

--- app/modelCore/models.py
+++ app/modelCore/models.py
@@ -149,10 +149,10 @@ class Order(models.Model):
     ATMInfovAccount = models.CharField(max_length=20, default='', blank = True, null=True)
     ATMInfoExpireDate = models.DateTimeField(auto_now=False, blank = True,null=True)
     
-    CVSInfoPayFrom = models.CharField(max_length=20, default='', blank = True, null=True)
     CVSInfoPaymentNo = models.CharField(max_length=20, default='', blank = True, null=True)
     CVSInfoPaymentURL = models.CharField(max_length=100, default='', blank = True, null=True)
-    
+    CVSInfoExpireDate = models.DateTimeField(auto_now=False, blank = True,null=True)
+
     createDate = models.DateTimeField(auto_now=False, blank = True,null=True)
 
 class ProductOrderShip(models.Model):
